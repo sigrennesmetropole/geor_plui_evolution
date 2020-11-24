033202367ee429057d0b54970b91d9369fbbdf5c
CAMARA <nakouban.rc@gmail.com>
2020-11-24 11:19:27 +0100

    Ajout des data par defaut de StatusEntity et de RequestTypeEntity

--- resources/sql/plui-evolution-rm-data.sql
+++ resources/sql/plui-evolution-rm-data.sql
@@ -1,2 +1,14 @@
-INSERT INTO signalement.user_ ( login, first_name, last_name, email) VALUES ( 't.brule', 'Thomas', 'Brulé', 't.brule@rennesmetropole.fr');
+INSERT INTO pluievolution.user_ ( login, first_name, last_name, email) VALUES ( 't.brule', 'Thomas', 'Brulé', 't.brule@rennesmetropole.fr');
 
+-- Insertion des enumération de Status
+INSERT INTO pluievolution.status (id, value) VALUES (1, 'STATUT_NOUVEAU');
+INSERT INTO pluievolution.status (id, value) VALUES (2, 'STATUT_ANALYSE_EN_COURS');
+INSERT INTO pluievolution.status (id, value) VALUES (3, 'STATUT_EN_ATTENTE_VALIDATION_COMMUNE');
+INSERT INTO pluievolution.status (id, value) VALUES (4, 'STATUT_VALIDE_COMMUNE');
+INSERT INTO pluievolution.status (id, value) VALUES (5, 'STATUT_DEMANDE_NON_RECEVABLE');
+INSERT INTO pluievolution.status (id, value) VALUES (6, 'STATUT_DEMANDE_REFORMULEE');
+
+-- Insertion des énumérations de PluiRequest
+INSERT INTO pluievolution.request_type (id, value) VALUES (1, 'TYPE_COMMUNE');
+INSERT INTO pluievolution.request_type (id, value) VALUES (2, 'TYPE_INTERCOMMUNE');
+INSERT INTO pluievolution.request_type (id, value) VALUES (3, 'TYPE_METROPOLITAIN');
