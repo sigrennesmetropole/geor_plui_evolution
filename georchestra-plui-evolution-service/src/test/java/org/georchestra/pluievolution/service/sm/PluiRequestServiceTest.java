bb42999042cd7da4a0e703b7c2ec89188396c278
NAKOUBAN RAOUL CAMARA <nakouban-raoul.camara@open-groupe.com>
2020-11-26 17:38:55 +0100

    TU ajout de demande

---
+++ georchestra-plui-evolution-service/src/test/java/org/georchestra/pluievolution/service/sm/PluiRequestServiceTest.java
@@ -0,0 +1,72 @@
+package org.georchestra.pluievolution.service.sm;
+
+import org.georchestra.pluievolution.StarterSpringBootTestApplication;
+import org.georchestra.pluievolution.core.dto.*;
+import org.georchestra.pluievolution.service.exception.ApiServiceException;
+import org.georchestra.pluievolution.service.ref.RefService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StarterSpringBootTestApplication.class)
+@ComponentScan({ "org.georchestra.pluievolution.api", "org.georchestra.pluievolution.service",
+        "org.georchestra.pluievolution.core" })
+@TestPropertySource(value = { "classpath:plui-evolution.properties", "classpath:plui-evolution-common.properties" })
+public class PluiRequestServiceTest {
+    @Autowired
+    PluiRequestService pluiRequestService;
+
+    @Autowired
+    RefService refService;
+
+    PluiRequest pluiRequest;
+
+    PluiRequest pluiRequest1;
+
+    private List<PluiRequestType> types;
+
+    private List<PluiRequestStatus> status;
+
+    @Before
+    public void init() {
+        pluiRequest = new PluiRequest();
+        pluiRequest.setType(refService.getRequestTypeByValue(PluiRequestTypeEnum.COMMUNE));
+        pluiRequest.setObject("Ici l'objet de la demande");
+        pluiRequest.setSubject("Ici le sujet de la demande");
+        Point2D pt2D = new Point2D();
+        pt2D.add(BigDecimal.valueOf(48.104594766322386));
+        pt2D.add(BigDecimal.valueOf(-1.666802393510429));
+        Point point = new Point();
+        point.setCoordinates(pt2D);
+        point.setType(GeometryType.POINT);
+        pluiRequest.setLocalisation(point);
+    }
+
+    @Test
+    public void createPluiRequest() throws ApiServiceException {
+        pluiRequest1 = pluiRequestService.createPluiRequest(pluiRequest);
+        Assert.assertNotNull("Le uuid ne doit être nul", pluiRequest1.getUuid());
+        Assert.assertNotNull("La localisation ne doit etre nulle", pluiRequest1.getLocalisation());
+        Assert.assertEquals("La localisation doit etre la meme que celle rensignee", pluiRequest.getLocalisation().hashCode(), pluiRequest1.getLocalisation().hashCode());
+        Assert.assertNotNull("La date de creation ne doit etre nulle", pluiRequest1.getCreationDate());
+
+        // Seulement si authentifié
+        // Assert.assertNotNull("L'initiateur de la demande doit avoir ete renseigné", pluiRequest1.getInitiator());
+        
+        Assert.assertEquals("L'objet de la demande doit le meme que celui renseigné", pluiRequest.getObject(), pluiRequest1.getObject());
+        Assert.assertEquals("Le sujet de la demade doit etre le meme que celui renseigné", pluiRequest.getSubject(), pluiRequest1.getSubject());
+
+        // On supprime la demande cree precedemment
+        pluiRequestService.deletePluiRequestByUuid(pluiRequest1.getUuid());
+    }
+}
