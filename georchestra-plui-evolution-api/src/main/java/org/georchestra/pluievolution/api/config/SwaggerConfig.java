87c9f17d71bb70243faa47cdb0e615889019fb0d
Alex SIEYADJEU <alex.sieyadjeu@open-groupe.com>
2020-12-08 16:52:00 +0100

    correction sonar

--- georchestra-plui-evolution-api/src/main/java/org/georchestra/pluievolution/api/config/SwaggerConfig.java
+++ georchestra-plui-evolution-api/src/main/java/org/georchestra/pluievolution/api/config/SwaggerConfig.java
@@ -1,13 +1,7 @@
 package org.georchestra.pluievolution.api.config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,6 +14,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -50,7 +48,7 @@ public class SwaggerConfig {
 	}
 
 	private List<SecurityReference> securityReferences() {
-		return Arrays.asList(new SecurityReference("adm", scopes()));
+		return Collections.singletonList(new SecurityReference("adm", scopes()));
 	}
 
 	private AuthorizationScope[] scopes() {
