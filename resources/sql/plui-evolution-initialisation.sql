3df8f21b1436919b1cc2de661219757d2b291709
NAKOUBAN RAOUL CAMARA <nakouban-raoul.camara@open-groupe.com>
2020-12-08 14:56:52 +0100

    MaJ du Script de creation de la table user_

--- resources/sql/plui-evolution-initialisation.sql
+++ resources/sql/plui-evolution-initialisation.sql
@@ -14,19 +14,29 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA pluievolution;
 -- DROP SEQUENCE pluievolution.user__id_seq;
 CREATE SEQUENCE pluievolution.user__id_seq;
 ALTER SEQUENCE pluievolution.user__id_seq OWNER TO pluievolution;
-    
+
+-- Table: pluievolution.user_
+
 -- DROP TABLE pluievolution.user_;
+
 CREATE TABLE pluievolution.user_
 (
-    id bigint NOT NULL DEFAULT nextval('pluievolution.user__id_seq'::regclass),
-    login character varying(100)  NOT NULL,
-    email character varying(150)  NOT NULL,
-    first_name character varying(150) ,
-    last_name character varying(150) ,
+    id bigint NOT NULL DEFAULT nextval('user__id_seq'::regclass),
+    email character varying(150) COLLATE pg_catalog."default" NOT NULL,
+    first_name character varying(150) COLLATE pg_catalog."default",
+    last_name character varying(150) COLLATE pg_catalog."default",
+    login character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    organization character varying(150) COLLATE pg_catalog."default",
+    roles character varying(1024) COLLATE pg_catalog."default",
     CONSTRAINT user__pkey PRIMARY KEY (id)
 )
-WITH (OIDS = FALSE);
-ALTER TABLE pluievolution.user_ OWNER to pluievolution;
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
+
+ALTER TABLE pluievolution.user_
+    OWNER to pluievolution;
 
 -- SEQUENCE: pluievolution.geographic_area_id_seq
 
