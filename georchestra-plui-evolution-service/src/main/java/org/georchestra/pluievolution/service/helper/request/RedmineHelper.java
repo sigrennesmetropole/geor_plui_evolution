cade401d72929469e2bf4c5e5af4cbb336255a7b
NAKOUBAN RAOUL CAMARA <nakouban-raoul.camara@open-groupe.com>
2020-12-11 15:20:54 +0100

    Envoi des demandes vers redmine, mises a jour des criteres de selection du sous proejet de destination

--- georchestra-plui-evolution-service/src/main/java/org/georchestra/pluievolution/service/helper/request/RedmineHelper.java
+++ georchestra-plui-evolution-service/src/main/java/org/georchestra/pluievolution/service/helper/request/RedmineHelper.java
@@ -3,15 +3,15 @@ package org.georchestra.pluievolution.service.helper.request;
 import com.taskadapter.redmineapi.*;
 import com.taskadapter.redmineapi.bean.*;
 import com.taskadapter.redmineapi.internal.Transport;
-import org.georchestra.pluievolution.core.dto.PluiRequestStatus;
 import org.georchestra.pluievolution.core.dto.PluiRequestType;
 import org.georchestra.pluievolution.core.entity.request.PluiRequestEntity;
+import org.georchestra.pluievolution.service.acl.GeographicAreaService;
+import org.georchestra.pluievolution.service.acl.GeographicEtablissementService;
 import org.georchestra.pluievolution.service.exception.ApiServiceException;
 import org.georchestra.pluievolution.service.helper.authentification.AuthentificationHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import java.text.Normalizer;
 import java.util.List;
 
 
@@ -19,20 +19,24 @@ import java.util.List;
 public class RedmineHelper {
 
     @Value("${redmine.uri}")
-    String uri;
+    private String uri;
 
     @Value("${redmine.api.access.key}")
-    String apiAccessKey;
+    private String apiAccessKey;
 
-    @Value("${redmine.project.key}")
-    String projectKey;
 
-    private static String CUSTOM_COLUMN_COMMENTAIRE = "commentaire"; // inutile
-    private static String CUSTOM_COLUMN_INITIATEUR = "initiateur";
-    private static String CUSTOM_COLUMN_TYPE = "type"; // inutile
+    private static final String CUSTOM_COLUMN_INITIATEUR = "initiateur";
+    private static final String FICTIVE_INTERCO_AREA_NAME = "Interco";
+    private static final String CODE_INSEE_RM = "243500139";
 
     @Autowired
     AuthentificationHelper authentificationHelper;
+
+    @Autowired
+    GeographicEtablissementService geographicEtablissementService;
+
+    @Autowired
+    GeographicAreaService geographicAreaService;
 
     /**
      * Permet de créer un ticket concernat la pluirequest dans redmine
@@ -42,68 +46,50 @@ public class RedmineHelper {
      * @throws ApiServiceException
      */
     public PluiRequestEntity createPluiRequestIssue(PluiRequestEntity pluiRequest) throws RedmineException, ApiServiceException {
-        Project projectByKey = null;
+        Project sousProjet = null;
         // On recupere le projet ou le sous projet vers lequel on veut envoyer la demande
         try {
-            String key = transformToIdentifiantFormat(pluiRequest.getArea().getNom());
-            projectByKey = getProjectManager().getProjectByKey(key);
+            String identifiant = null;
+            if (pluiRequest.getType() == PluiRequestType.COMMUNE) {
+                // si type commune alors sous projet remine de la commune
+                identifiant = pluiRequest.getArea().getIdentifiantRedmine();
+            } else if (pluiRequest.getType() == PluiRequestType.INTERCOMMUNE) {
+                // si type intercommune, sous projet redmine pour les demandes intercommunes
+                identifiant = geographicAreaService.getGeographicAreaByNom(FICTIVE_INTERCO_AREA_NAME).getIdentifiantRedmine();
+            } else if (pluiRequest.getType() == PluiRequestType.METROPOLITAIN) {
+                // si type metropolitain alors sous projet redmine pour les demandes metropolitaines
+                identifiant = geographicAreaService.getGeographicAreaByCodeInsee(CODE_INSEE_RM).getIdentifiantRedmine();
+            }
+
+            sousProjet = getProjectManager().getProjectByKey(identifiant);
         } catch (RedmineException e) {
             throw new RedmineException(e.getMessage());
         }
 
         // Creation de l'issue
-        Issue issue = new Issue(getTransport(), projectByKey.getId())
+        Issue issue = new Issue(getTransport(), sousProjet.getId())
                 .setSubject(pluiRequest.getSubject())
                 .setCreatedOn(pluiRequest.getCreationDate())
-                .setStatusName(pluiRequest.getStatus().toString())
                 .setDescription(pluiRequest.getObject());
+
         // Ajout des valeurs des champs customs
-
-        // Ajout du type de demande
-        CustomFieldDefinition cfd = getCustomFieldByName(CUSTOM_COLUMN_TYPE);
-        if ( cfd != null ) {
-            issue.addCustomField(CustomFieldFactory.create(cfd.getId(), cfd.getName(), pluiRequest.getType().toString()));
-        }
-
         // Ajout de l'initiateur de la demande
-        cfd = getCustomFieldByName(CUSTOM_COLUMN_INITIATEUR);
+        CustomFieldDefinition cfd = getCustomFieldByName(CUSTOM_COLUMN_INITIATEUR);
         if ( cfd != null ) {
             issue.addCustomField(CustomFieldFactory.create(cfd.getId(), cfd.getName(), pluiRequest.getInitiator()));
         }
 
         // Creation de l'issue dans redmine
-        Issue sent = issue.create();
-
-        // recuperation de l'id du ticket créé dans redmine
-        if (sent != null) {
-            pluiRequest.setRedmineId(sent.getId().toString());
+        try {
+            Issue sent = issue.create();
+            // recuperation de l'id du ticket créé dans redmine
+            pluiRequest.setRedmineId(sent.getId());
+            return pluiRequest;
+        } catch (RedmineException e) {
+            throw new ApiServiceException(e.getMessage());
         }
-
-        return pluiRequest;
     }
 
-    /**
-     * Permet de synchroniser une demande avec les infos contenue dans son ticket redmine
-     * @param pluiRequest
-     * @return
-     * @throws RedmineException
-     * @throws ApiServiceException
-     */
-    public PluiRequestEntity updatePluiRequestFromRedmine(PluiRequestEntity pluiRequest) throws RedmineException, ApiServiceException {
-        if (pluiRequest.getRedmineId() == null) {
-            throw new ApiServiceException("Aucun ticket correspondant dans redmine");
-        }
-        Issue issue = getIssueByRedmineId(Integer.valueOf(pluiRequest.getRedmineId()));
-        pluiRequest.setSubject(issue.getSubject());
-        pluiRequest.setObject(issue.getDescription());
-        pluiRequest.setStatus(PluiRequestStatus.fromValue(issue.getStatusName()));
-        pluiRequest.setCreationDate(issue.getCreatedOn());
-        pluiRequest.setType(PluiRequestType.fromValue(issue.getCustomFieldByName(CUSTOM_COLUMN_TYPE).getValue()));
-        pluiRequest.setInitiator(issue.getCustomFieldByName(CUSTOM_COLUMN_INITIATEUR).getValue());
-        pluiRequest.setComment(issue.getCustomFieldByName(CUSTOM_COLUMN_COMMENTAIRE).getValue());
-
-        return pluiRequest;
-    }
 
     /**
      * Permet de recuperer une issue redmine à partir de son id
@@ -119,6 +105,20 @@ public class RedmineHelper {
             throw new ApiServiceException(e.getMessage());
         }
 
+    }
+
+    /**
+     * Permet de supprimer un ticket dans redmine
+     * @param id
+     * @return
+     */
+    public void deleteIssueById(Integer id) throws RedmineException, ApiServiceException {
+        Issue issue = getIssueByRedmineId(id);
+        try {
+            issue.delete();
+        } catch (RedmineException e) {
+            throw new ApiServiceException(e.getMessage(), e);
+        }
     }
 
     /**
@@ -168,23 +168,4 @@ public class RedmineHelper {
         RedmineManager mgr = getRedmineManager();
         return mgr.getProjectManager();
     }
-
-    /**
-     * Transforme le nom de la geographic area en un format compatible avec les identifiants a-z-_
-     * @param nom
-     * @return
-     */
-    private String transformToIdentifiantFormat(String nom) {
-        // On remplace tous les caracteres avec accents par leurs equivalent en caracteres ascii
-        String identifiant = Normalizer
-                .normalize(nom, Normalizer.Form.NFD)
-                .replaceAll("[^\\p{ASCII}]", "");
-        // On remplace espaces et apostrophes par des -
-        identifiant = identifiant.replace('\'', '-');
-        identifiant = identifiant.replace(' ', '-');
-        return identifiant.toLowerCase(); // on la lowercase de chaine de caracteres
-    }
-
-
-
 }
