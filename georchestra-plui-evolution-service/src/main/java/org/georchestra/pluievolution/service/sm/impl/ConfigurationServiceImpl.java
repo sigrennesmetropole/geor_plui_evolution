73cc8cfa0ebe2df89d20d6c882e5350f6134528b
NAKOUBAN RAOUL CAMARA <nakouban-raoul.camara@open-groupe.com>
2021-02-08 00:35:04 +0100

    API Recuperation des configurations du layer

--- georchestra-plui-evolution-service/src/main/java/org/georchestra/pluievolution/service/sm/impl/ConfigurationServiceImpl.java
+++ georchestra-plui-evolution-service/src/main/java/org/georchestra/pluievolution/service/sm/impl/ConfigurationServiceImpl.java
@@ -1,5 +1,6 @@
 package org.georchestra.pluievolution.service.sm.impl;
 
+import org.georchestra.pluievolution.core.dto.LayerConfiguration;
 import org.georchestra.pluievolution.service.bean.Configuration;
 import org.georchestra.pluievolution.core.dto.ConfigurationData;
 import org.georchestra.pluievolution.service.mapper.ConfigurationMapper;
@@ -14,11 +15,25 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     @Value("${application.version}")
     private String version;
 
+    @Value("${pluievolution.geoserver.defaultworkspace}")
+    private String layerWorkspace;
+
+    @Value("${pluievolution.geoserver.layerName}")
+    private String layerName;
+
     @Autowired
     ConfigurationMapper configMapper;
 
     @Override
     public ConfigurationData getApplicationVersion() {
         return configMapper.entityToDto(new Configuration(version));
+    }
+
+    @Override
+    public LayerConfiguration getLayerConfigurations() {
+        LayerConfiguration layerConfiguration = new LayerConfiguration();
+        layerConfiguration.setLayerName(layerName);
+        layerConfiguration.setLayerWorkspace(layerWorkspace);
+        return layerConfiguration;
     }
 }
