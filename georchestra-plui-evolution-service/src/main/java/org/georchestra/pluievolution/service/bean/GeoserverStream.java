6717436f857d1e1f4efa5a26fa6ff2f6d3b8de75
fni18300 <fni18300@open-groupe.com>
2021-11-09 18:10:55 +0100

    Amélioration du passthrougt de geoserverservice

--- georchestra-plui-evolution-service/src/main/java/org/georchestra/pluievolution/service/bean/GeoserverStream.java
+++ georchestra-plui-evolution-service/src/main/java/org/georchestra/pluievolution/service/bean/GeoserverStream.java
@@ -3,14 +3,17 @@
  */
 package org.georchestra.pluievolution.service.bean;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 /**
  * @author FNI18300
@@ -21,13 +24,24 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@ToString(exclude = {"stream"})
 public class GeoserverStream {
 
 	private InputStream stream;
 
+	private String content;
+
 	private String mimeType;
 
 	private int status;
+
+	public String getContent() throws IOException {
+		if (content != null) {
+			return content;
+		} else if (stream != null) {
+			return IOUtils.toString(getStream(), StandardCharsets.UTF_8);
+		} else {
+			return null;
+		}
+	}
 
 }
