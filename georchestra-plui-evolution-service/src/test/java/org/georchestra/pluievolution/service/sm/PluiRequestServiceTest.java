4829a85ff98e9c56fdb9f97c9247f827f6bc1fcc
fni18300 <fni18300@open-groupe.com>
2022-04-06 14:39:08 +0200

    --

--- georchestra-plui-evolution-service/src/test/java/org/georchestra/pluievolution/service/sm/PluiRequestServiceTest.java
+++ georchestra-plui-evolution-service/src/test/java/org/georchestra/pluievolution/service/sm/PluiRequestServiceTest.java
@@ -1,78 +1,87 @@
 package org.georchestra.pluievolution.service.sm;
 
-import org.georchestra.pluievolution.StarterSpringBootTestApplication;
-import org.georchestra.pluievolution.core.dto.*;
-import org.georchestra.pluievolution.core.entity.request.PluiRequestEntity;
-import org.georchestra.pluievolution.service.mapper.PluiRequestMapper;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
 
-@RunWith(SpringRunner.class)
+import org.georchestra.pluievolution.StarterSpringBootTestApplication;
+import org.georchestra.pluievolution.core.dto.GeometryType;
+import org.georchestra.pluievolution.core.dto.PluiRequest;
+import org.georchestra.pluievolution.core.dto.PluiRequestStatus;
+import org.georchestra.pluievolution.core.dto.PluiRequestType;
+import org.georchestra.pluievolution.core.dto.Point;
+import org.georchestra.pluievolution.core.dto.Point2D;
+import org.georchestra.pluievolution.core.entity.request.PluiRequestEntity;
+import org.georchestra.pluievolution.service.mapper.PluiRequestMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+
 @SpringBootTest(classes = StarterSpringBootTestApplication.class)
 @ComponentScan({ "org.georchestra.pluievolution.api", "org.georchestra.pluievolution.service",
-        "org.georchestra.pluievolution.core" })
-@EntityScan({"org.georchestra.pluievolution.core.entity"})
+		"org.georchestra.pluievolution.core" })
+@EntityScan({ "org.georchestra.pluievolution.core.entity" })
 @TestPropertySource(value = { "classpath:plui-evolution.properties", "classpath:plui-evolution-common.properties" })
-public class PluiRequestServiceTest {
-    @Autowired
-    PluiRequestService pluiRequestService;
+class PluiRequestServiceTest {
+	@Autowired
+	PluiRequestService pluiRequestService;
 
-    @Autowired
-    PluiRequestMapper pluiRequestMapper;
+	@Autowired
+	PluiRequestMapper pluiRequestMapper;
 
-    PluiRequest pluiRequest;
+	PluiRequest pluiRequest;
 
-    PluiRequest pluiRequest1;
+	PluiRequest pluiRequest1;
 
-    @Before
-    public void init() {
-        pluiRequest = new PluiRequest();
-        pluiRequest.setType(PluiRequestType.COMMUNE);
-        pluiRequest.setObject("Ici l'objet de la demande");
-        pluiRequest.setSubject("Ici le sujet de la demande");
-        Point2D pt2D = new Point2D();
-        pt2D.add(BigDecimal.valueOf(48.104594766322386));
-        pt2D.add(BigDecimal.valueOf(-1.666802393510429));
-        Point point = new Point();
-        point.setCoordinates(pt2D);
-        point.setType(GeometryType.POINT);
-        pluiRequest.setLocalisation(point);
-        pluiRequest.setCreationDate(new Date());
-        pluiRequest.setUuid(UUID.randomUUID());
-        pluiRequest.setStatus(PluiRequestStatus.NOUVEAU);
-    }
+	@BeforeAll
+	public void init() {
+		pluiRequest = new PluiRequest();
+		pluiRequest.setType(PluiRequestType.COMMUNE);
+		pluiRequest.setObject("Ici l'objet de la demande");
+		pluiRequest.setSubject("Ici le sujet de la demande");
+		Point2D pt2D = new Point2D();
+		pt2D.add(BigDecimal.valueOf(48.104594766322386));
+		pt2D.add(BigDecimal.valueOf(-1.666802393510429));
+		Point point = new Point();
+		point.setCoordinates(pt2D);
+		point.setType(GeometryType.POINT);
+		pluiRequest.setLocalisation(point);
+		pluiRequest.setCreationDate(new Date());
+		pluiRequest.setUuid(UUID.randomUUID());
+		pluiRequest.setStatus(PluiRequestStatus.NOUVEAU);
+	}
 
-    @Test
-    public void mappingPluiRequest() {
-        PluiRequestEntity pluiRequestEntity = pluiRequestMapper.dtoToEntity(pluiRequest);
-        Assert.assertNull("La date de creation doit etre nulle", pluiRequestEntity.getCreationDate());
-        pluiRequestEntity.setCreationDate(new Date());
-        pluiRequest1 = pluiRequestMapper.entityToDto(pluiRequestEntity);
-        Assert.assertNotNull("Le uuid ne doit être nul", pluiRequest1.getUuid());
-        Assert.assertNotNull("La localisation ne doit etre nulle", pluiRequest1.getLocalisation());
-        Assert.assertEquals("La localisation doit etre la meme que celle rensignee", pluiRequest.getLocalisation().hashCode(), pluiRequest1.getLocalisation().hashCode());
-        Assert.assertNotNull("La date de creation ne doit etre nulle", pluiRequest1.getCreationDate());
-        Assert.assertNotNull("Le type de demande doit etre renseigné", pluiRequest1.getType());
-        Assert.assertNotNull("Le status de demande doit etre renseigné", pluiRequest1.getStatus());
+	@Test
+	void mappingPluiRequest() {
+		PluiRequestEntity pluiRequestEntity = pluiRequestMapper.dtoToEntity(pluiRequest);
+		assertNull(pluiRequestEntity.getCreationDate(), "La date de creation doit etre nulle");
+		pluiRequestEntity.setCreationDate(new Date());
+		pluiRequest1 = pluiRequestMapper.entityToDto(pluiRequestEntity);
+		assertNotNull(pluiRequest1.getUuid(), "Le uuid ne doit être nul");
+		assertNotNull(pluiRequest1.getLocalisation(), "La localisation ne doit etre nulle");
+		assertEquals(pluiRequest.getLocalisation().hashCode(), pluiRequest1.getLocalisation().hashCode(),
+				"La localisation doit etre la meme que celle rensignee");
+		assertNotNull(pluiRequest1.getCreationDate(), "La date de creation ne doit etre nulle");
+		assertNotNull(pluiRequest1.getType(), "Le type de demande doit etre renseigné");
+		assertNotNull(pluiRequest1.getStatus(), "Le status de demande doit etre renseigné");
 
-        // Seulement si authentifié
-        // Assert.assertNotNull("L'initiateur de la demande doit avoir ete renseigné", pluiRequest1.getInitiator());
-        
-        Assert.assertEquals("L'objet de la demande doit le meme que celui renseigné", pluiRequest.getObject(), pluiRequest1.getObject());
-        Assert.assertEquals("Le sujet de la demade doit etre le meme que celui renseigné", pluiRequest.getSubject(), pluiRequest1.getSubject());
+		// Seulement si authentifié
+		// assertNotNull("L'initiateur de la demande doit avoir ete renseigné",
+		// pluiRequest1.getInitiator());
 
-    }
+		assertEquals("L'objet de la demande doit le meme que celui renseigné", pluiRequest.getObject(),
+				pluiRequest1.getObject());
+		assertEquals("Le sujet de la demade doit etre le meme que celui renseigné", pluiRequest.getSubject(),
+				pluiRequest1.getSubject());
+
+	}
 
 }
